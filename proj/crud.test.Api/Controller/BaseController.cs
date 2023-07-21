4f0da79020b48374ccfe5e960aea0c91d5ef89b5
laptop <kambiz.a.babaei@gmail.com>
2023-07-21 20:53:44 +0330

    Api:
    - added base controller
    - added Product Controller

---
+++ proj/crud.test.Api/Controller/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud.test.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseController : ControllerBase
+{
+    protected ActionResult<TResult> OkOrNotFound<TResult>(TResult result)
+    {
+        return result is null ? NotFound() : Ok(result);
+    }
+}
