216e98ef7ea7d4907ab7869308ce561cd74cabde
laptop <kambiz.a.babaei@gmail.com>
2023-07-21 19:13:09 +0330

    command and queries
    infrastructure:
    - added get product by id query handler
    - added search product by name query handler
    - added product dto
    - added product read service

---
+++ proj/crud.test.Infrastructure/EF/Queries/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using crud.test.Application.Dtoes;
+using crud.test.Infrastructure.EF.Models;
+
+namespace crud.test.Infrastructure.EF.Queries;
+
+internal static class Extensions
+{
+    public static ProductDto AsDto(this ProductReadModel readModel)
+    {
+        return new ProductDto
+        {
+            Id = readModel.Id,
+            Name = readModel.Name,
+            IsAvailable = readModel.IsAvailable,
+            ManufactureEmail = readModel.ManufactureEmail,
+            ManufacturePhone = readModel.ManufacturePhone,
+            ProduceDate = readModel.ProduceDate
+        };
+    }
+}
---
+++ proj/crud.test.Infrastructure/EF/Queries/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using crud.test.Abstraction.Queries;
+using crud.test.Application.Dtoes;
+using crud.test.Application.Queries;
+using test.Application.Services;
+
+namespace crud.test.Infrastructure.EF.Queries.Handlers;
+
+public class GetProductByIdQueryHandler : IQueryHandler<GetProductByIdQuery, ProductDto>
+{
+    private readonly IProductReadServices _readServices;
+
+    public GetProductByIdQueryHandler(IProductReadServices repository)
+    {
+        _readServices = repository;
+    }
+
+    public async Task<ProductDto> HandleAsync(GetProductByIdQuery query)
+    {
+        var product = await _readServices.GetAsync(query.Id);
+        return product;
+    }
+}
