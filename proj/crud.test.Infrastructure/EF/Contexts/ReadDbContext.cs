13128f84d63182d36846dfc65b64353028bc2b3e
laptop <kambiz.a.babaei@gmail.com>
2023-07-21 17:11:47 +0330

    domain: - added product repository interface
    infrastructure:
    - added ReadConfigs
    - added write configs
    - added read db context
    - added write db context
    - added product repository

---
+++ proj/crud.test.Infrastructure/EF/Contexts/ReadDbContext.cs
@@ -0,0 +1,21 @@
+﻿using crud.test.Infrastructure.EF.Configurations;
+using crud.test.Infrastructure.EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.test.Infrastructure.EF.Contexts;
+
+internal sealed class ReadDbContext : DbContext
+{
+    public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ProductReadModel> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("Products");
+        var Configuration = new ReadConfigs();
+        modelBuilder.ApplyConfiguration<ProductReadModel>(Configuration);
+    }
+}
---
+++ proj/crud.test.Infrastructure/EF/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using crud.test.Domain.Domain;
+using crud.test.Domain.Repositories;
+using crud.test.Domain.ValueObjects.Product;
+using crud.test.Infrastructure.EF.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.test.Infrastructure.EF.Repositories;
+
+internal sealed class ProductRepository : IProductRepository
+{
+    private readonly DbSet<Product> _products;
+    private readonly WriteDbContext _writeDbContext;
+
+
+    public ProductRepository(DbSet<Product> products, WriteDbContext writeDbContext)
+    {
+        _products = products;
+        _writeDbContext = writeDbContext;
+    }
+
+
+    public async Task<Product?> GetAsync(ProductId id)
+    {
+        return await _products.SingleOrDefaultAsync(product => product.Id == id);
+    }
+
+    public async Task<Product> AddAsync(Product product)
+    {
+        var productEntity = await _products.AddAsync(product);
+        return productEntity.Entity;
+    }
+
+    public Task UpdateAsync(Product product)
+    {
+        _products.Update(product);
+        return Task.CompletedTask;
+    }
+
+
+    public Task DeleteAsync(Product product)
+    {
+        _products.Remove(product);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _writeDbContext.SaveChangesAsync();
+    }
+}
