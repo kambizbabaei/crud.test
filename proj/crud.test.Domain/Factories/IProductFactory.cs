c76077524a202c5b6c25e5464d26241c0f447cae
laptop <kambiz.a.babaei@gmail.com>
2023-07-22 13:29:34 +0330

    factories
    - added product factory

---
+++ proj/crud.test.Domain/Factories/IProductFactory.cs
@@ -0,0 +1,13 @@
+﻿using crud.test.Domain.Domain;
+
+namespace crud.test.Domain.Factories;
+
+public interface IProductFactory
+{
+    Product Create(bool IsAvailable, string ManufactureEmail, string ManufacturePhone, DateTime ProduceDate,
+        string Name);
+
+    Product Create(
+        (bool IsAvailable, string ManufactureEmail, string ManufacturePhone, DateTime ProduceDate, string Name)
+            ProductData);
+}
